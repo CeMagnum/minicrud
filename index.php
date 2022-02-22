2716156acfd5a9b20b371ca2e8b4b2d669821f78
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-02-22 10:51:01 +0100

    setup

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Import css en java -->
+    <script src="./assets/js/main.js" async></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <!-- Titel en favicon-->
+    <title>porto2.0</title>
+    <link rel="shortcut icon" href="./assets/img/favicon.ico">
+    
+</head>
+<body>
+
+    
+</body>
+</html>
