3c9500c33e0f9ccaf42d1b061848b06150445116
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-02-23 19:28:34 +0100

    kaas

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
         // var_dump($result);
         foreach($result as $i)
         {echo "<div><h2>".$i['ID']."<h2><br>".$i['naam']."<br><img src=\"".$i['imgsource']."\" alt='".$i['beschrijving']."' /><br>Categorie: ".$i['categorie']."<br>".$i['beschrijving']."<a href=\"product.php?product_id=".$i['ID']."\"><br>bekijk product</a></div>";}?>
-
     </main>
 
     <?php include('footer.php');?>
