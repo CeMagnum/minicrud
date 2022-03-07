4261a20e51d8a7ac95052e0522258fc962893f47
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-03-07 16:15:19 +0100

    jyjy

--- create.php
+++ create.php
@@ -1,12 +1,7 @@
-<?php include('header.php');?>
-<form action="welcome.php" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    <input type="text" name="prijs" placeholder="Prijs (double)">
-    <input type="text" name="naam" placeholder="Naam (varchar)">
-    <input type="text" name="imgsource" placeholder="Locatie afbeelding (varchar)">
-    <input type="text" name="categorie" placeholder="Categorie (varchar)">
-    <input type="text" name="beschrijving" placeholder="Beschrijving (varchar)">
-    <input type="submit">
-</form>
+<?php
+include('header.php');
+include('form.php');
+?>
 
 <?php
 
@@ -16,14 +11,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $imgsource = $_POST['imgsource'];
     $categorie = $_POST['categorie'];
     $beschrijving = $_POST['beschrijving'];
-    $sql = "INSERT INTO menukaart (prijs, naam, imgsource, categorie, beschrijving) VALUES ($prijs, $naam, $imgsource, $categorie, $beschrijving)";
+    $sql = "INSERT INTO menukaart (prijs, naam, imgsource, categorie, beschrijving) VALUES ($prijs, '$naam', '$imgsource', '$categorie', '$beschrijving')";
+    
     if (empty($prijs || $naam || $imgsource || $categorie || $beschrijving)) {
         echo "Vul alle velden in!";
     } else {
         echo 'Compleet!';
+        $test = $conn->query($sql);
+        var_dump($test);
+
+        $conn->prepare($sql)->execute($test);
     }
 }
 
 ?>
-
     <?php include('footer.php');?>
---
+++ form.php
@@ -0,0 +1,8 @@
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <input type="text" name="prijs" placeholder="Prijs (double)">
+    <input type="text" name="naam" placeholder="Naam (varchar)">
+    <input type="text" name="imgsource" placeholder="Locatie afbeelding (varchar)">
+    <input type="text" name="categorie" placeholder="Categorie (varchar)">
+    <input type="text" name="beschrijving" placeholder="Beschrijving (varchar)">
+    <input type="submit">
+</form>
