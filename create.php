e2030def6f79daa7b8d442b3328c6684ea0ab8a6
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-03-09 09:50:20 +0100

    dsjklfsdfkjsdfl

--- create.php
+++ create.php
@@ -6,23 +6,39 @@ include('form.php');
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $prijs = $_POST['prijs'];
-    $naam = $_POST['naam'];
-    $imgsource = $_POST['imgsource'];
-    $categorie = $_POST['categorie'];
+    $prijs =        $_POST['prijs'];
+    $naam =         $_POST['naam'];
+    $imgsource =    $_POST['imgsource'];
+    $categorie =    $_POST['categorie'];
     $beschrijving = $_POST['beschrijving'];
     $sql = "INSERT INTO menukaart (prijs, naam, imgsource, categorie, beschrijving) VALUES ($prijs, '$naam', '$imgsource', '$categorie', '$beschrijving')";
     
     if (empty($prijs || $naam || $imgsource || $categorie || $beschrijving)) {
         echo "Vul alle velden in!";
-    } else {
+    } else if (isset($_POST['knop'])) {
         echo 'Compleet!';
-        $test = $conn->query($sql);
-        var_dump($test);
+        // $test = $conn->query($sql);
+        // var_dump($test);
 
         $conn->prepare($sql)->execute($test);
+    } else {
+        echo 'idk what happened';
+        // var_dump($test);
     }
+} else {
+    echo "1";
 }
 
 ?>
     <?php include('footer.php');?>
+<?php 
+
+// $naam = $_POST['voornaam']
+
+if (isset($_POST['knop'])) {
+    $naam = $_POST['voornaam'];
+}
+
+
+
+?>
--- form.php
+++ form.php
@@ -1,8 +1,10 @@
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    <input type="text" name="prijs" placeholder="Prijs (double)">
-    <input type="text" name="naam" placeholder="Naam (varchar)">
-    <input type="text" name="imgsource" placeholder="Locatie afbeelding (varchar)">
-    <input type="text" name="categorie" placeholder="Categorie (varchar)">
-    <input type="text" name="beschrijving" placeholder="Beschrijving (varchar)">
-    <input type="submit">
+<form method="post" action="create.php">
+    <input type="text" name="prijs"         placeholder="Prijs (double)">
+    <input type="text" name="naam"          placeholder="Naam (varchar)">
+    <input type="text" name="imgsource"     placeholder="Locatie afbeelding (varchar)">
+    <input type="text" name="categorie"     placeholder="Categorie (varchar)">
+    <input type="text" name="beschrijving"  placeholder="Beschrijving (varchar)">
+    <input type="submit" value="verzenden" name="knop">
 </form>
+
+<?php# echo $_SERVER['PHP_SELF'];?>
