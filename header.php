83bc79814a6998707efc7b30789a2ef1d47f5be5
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-03-14 21:35:34 +0100

    style

--- header.php
+++ header.php
@@ -36,6 +36,6 @@
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
         } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();}?>
-
+    echo "Connection failed: " . $e->getMessage();
+    }?>
     </header>
