f32ced1ef258da8be205095f73c8e23d625026d4
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-03-14 13:55:47 +0100

    edit page

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php include('header.php') ?>
     <?php
-        if(isset($_POST['verwijderItem'])){
-            $stm = $conn->prepare("DELETE FROM menukaart WHERE ID = :product_id");
+        if(isset($_POST['editItem'])){
+            $stm = $conn->prepare("UPDATE menukaart SET lastname='Doe' WHERE id=:product_id");
             $stm->bindParam(":product_id", $_POST['product_id'], PDO::PARAM_INT);
             $stm->execute();
         }
--- product.php
+++ product.php
@@ -1,25 +1,22 @@
 <?php include('header.php');?>
-<?php //var_dump($_GET);?>
     <main>
         <?php
         $stm = $conn->prepare("SELECT * FROM menukaart WHERE ID = :product_id");
         $stm->bindParam(":product_id", $_GET['product_id'], PDO::PARAM_INT);
         $stm->execute();
-
-
         $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-        // var_dump($result);
         foreach($result as $i)
         {echo "<div><h2>".$i['prijs']."<h2><br>".$i['naam']."<br><img src=\"".$i['imgsource']."\" alt='".$i['beschrijving']."' /><br>Categorie: ".$i['categorie']."<br>".$i['beschrijving']."</div>";}?>
-        <form method="post" action="create.php">
+        <form method="POST" action="edit.php">
             <input type="text" name="prijs"         placeholder="Prijs (double)">
             <input type="text" name="naam"          placeholder="Naam (varchar)">
             <input type="text" name="imgsource"     placeholder="Locatie afbeelding (varchar)">
             <input type="text" name="categorie"     placeholder="Categorie (varchar)">
             <input type="text" name="beschrijving"  placeholder="Beschrijving (varchar)">
+            <button type="submit" name="editItem" value="Edit item">Push edit</button>
         </form>
         <?php #$sql = "UPDATE $result SET lastname='Doe';" ?>
-
+        
 
 
 
@@ -27,8 +24,8 @@
 
         <form action="verwijder.php" method="POST">
             <input class="onzichtbaar" type="text" value="<?php echo $_GET['product_id']?>" name="product_id" />
-    <button type="submit" name="verwijderItem" value="Verwijder item">Delete</button>
-</form>
+            <button type="submit" name="verwijderItem" value="Verwijder item">Delete</button>
+        </form>
     </main>
 
     <?php include('footer.php');?>
