90e3ed024dae093b3d0a93d48ffbd5112e90ff89
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-03-15 11:13:59 +0100

    css styling

--- product.php
+++ product.php
@@ -11,9 +11,9 @@
 
         <!-- BEWERKINGS FORM -->
         <form method="POST" action="edit.php">
-            <input type="text" name="prijs"         placeholder="Prijs (double)"                value="<?php echo $prijs?>">
-            <input type="text" name="naam"          placeholder="Naam (varchar)"                value="<?php echo $naam?>">
-            <input type="text" name="imgsource"     placeholder="Locatie afbeelding (varchar)"  value="<?php echo $imgsource?>">
+            <input type="text" name="prijs"         placeholder="Prijs (double)"                value="<?php echo $i['prijs']?>">
+            <input type="text" name="naam"          placeholder="Naam (varchar)"                value="<?php echo $i['naam']?>">
+            <input type="text" name="imgsource"     placeholder="Locatie afbeelding (varchar)"  value="<?php echo $i['imgsource']?>">
             <!-- <input type="text" name="categorie"     placeholder="Categorie (varchar)"> -->
             <select name="categorie" id="categorie">
                 <option value="dranken">Dranken</option>
@@ -21,7 +21,7 @@
                 <option value="hoofdgerechten">Hoofdgerechten</option>
                 <option value="nagerechten">Nagerechten</option>
             </select>
-            <input type="text" name="beschrijving"  placeholder="Beschrijving (varchar)"        value="<?php echo $beschrijving?>">
+            <input type="text" name="beschrijving"  placeholder="Beschrijving (varchar)"        value="<?php echo $i['beschrijving']?>">
             <input class="onzichtbaar" type="text" value="<?php echo $_GET['product_id']?>" name="product_id" />
             <button type="submit" name="editItem" value="Edit item">Push edit</button>
         </form>
